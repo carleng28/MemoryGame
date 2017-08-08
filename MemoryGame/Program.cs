bb98d43fbc7ee0c4af6bbe345455733d6410db54
Carlos Gomez <carleng28@gmail.com>
2017-08-08 17:02:07 -0400

    singleton pattern for MemoryGame class. Three data structures used

--- MemoryGame/Program.cs
+++ MemoryGame/Program.cs
@@ -16,7 +16,7 @@ namespace MemoryGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new user());
+            Application.Run(new UserSelect());
         }
     }
 }
